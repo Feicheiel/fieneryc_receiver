d9ea5245e33188e03d08d21ce581ae68b538b9ca
Eicheiel <feicheiel.gh@gmail.com>
2024-07-11 11:43:07 +0000

    v3.2: Major changes made to provide support for the RYLR993 communication protocol.

--- receiver_v3.1.ino
+++ receiver_v3.1.ino
@@ -26,9 +26,13 @@ SoftwareSerial Fieneryc(8,9);
 char f_read = ' ';
 char s_read = ' ';
 
-String stringSRead = "";
-String stringFRead = "";
-String sendCommand = "AT+SEND=3,8,B0\r\n";
+String stringSRead      = "";
+String stringFRead      = "";
+String sendCommand      = "";
+String THIS_RCVR_ADDR   = "AT+ADDRESS=11317\r\n";
+String OTHER_SNDR_ADDR  = "AT+ADDRESS=11731\r\n";
+String PARAMETER        = "AT+PARAMETER=7,9,3,13\r\n";
+const String sendAddr   = "AT+SEND=11731,8,";
 
 int isSRead = 0, isFRead = 0, isLedOn = 0;
 uint_8t MODE = 0;
@@ -37,22 +41,35 @@ unsigned long sendTime = millis(),
               serialUpdateTime = millis(), 
               fienerycUpdateTime = millis(), 
               lastReadFieneryc = millis(),
-              lastWrite = millis();
+              lastWrite = millis(), 
+              prototypeUpdateTime = millis();
               
-unsigned int updateTime = 500, ledBlinkTime = 300;
+const unsigned int updateTime = 500, ledBlinkTime = 300, prototypeTime = 1000, pctr=1;
 
 
 void setup() {
   Serial.begin(9600);
   Fieneryc.begin(9600);
+  delay(1);
 
   //Setup comm
-  Fieneryc.print("AT+ADDRESS=6\r\n");
-  Fieneryc.print("AT+PARAMETER=7,3,4,5\r\n");
+  while(!Fieneryc);
+  Fieneryc.print("AT\r\n");
+  delay(1);
+  Fieneryc.print("AT+OPMODE=1\r\n"); //For RYLR993 you need to set it to 1 to use the RYLR998 syntax, with a default NETWORKID=18
+  delay(1);
+  Fieneryc.print("AT+BAND=915000000\r\n");
+  delay(1);
+  Fieneryc.print(PARAMETER);
+  delay(1);
+  Fieneryc.print(THIS_RCVR_ADDR); // Set the device address for this receiver: 11317.
+  delay(1);
+  Fieneryc.print("AT+CPIN=F7C3901E\r\n");
+  delay(1);
 
   pinMode(GREEN, OUTPUT);
   led_set;
-  
+
 }
 
 void readSerial(){
@@ -62,7 +79,7 @@ void readSerial(){
       stringSRead += s_read; 
       isSRead++;
     }    
-   }
+  }
 }
 
 void readFieneryc(){
@@ -75,56 +92,66 @@ void readFieneryc(){
 
 void loop() {
 
-   if(millis() - serialUpdateTime >= updateTime){
+  if(millis() - serialUpdateTime >= updateTime){
     readSerial();
     serialUpdateTime = millis();
-   }
+  }
 
-   if(isSRead) {    
-    Serial.print("Serial:|(");
-    Serial.print(isSRead); Serial.print(" bytes read)|");
-    Serial.println(stringSRead);
-    stringSRead += "\r\n";
-    Fieneryc.print(stringSRead);
+  if(isSRead) {
+    //check the mode that is set.
 
-    //if (stringSRead.equals("+OK")){
-      //
-    //}
+    //set the mode
+    if (!MODE) {//Normal Mode
+      Serial.print("Serial:|(");
+      Serial.print(isSRead); Serial.print(" bytes read)|");
+      Serial.println(stringSRead);
+      stringSRead += "\r\n";
+      Fieneryc.print(stringSRead);
+    }
+    else if (MODE == 5) { //Set Parameter Mode
+        stringSRead += "\r\n";
+        Fieneryc.print(stringSRead);      
+    }    
     isSRead = 0; stringSRead = "";
-    
-   }
+  }
 
-   if(millis() - fienerycUpdateTime >= updateTime){
-    
+  if(millis() - fienerycUpdateTime >= updateTime){    
     readFieneryc();
-    fienerycUpdateTime = millis();
-    
-   }
+    fienerycUpdateTime = millis();    
+  }
 
-   if (isFRead){
+  if (isFRead){
     lon; isLedOn = 1;
     Serial.print("Fieneryc:|(");
     Serial.print(isFRead); Serial.print(" bytes read)|");
     Serial.println(stringFRead);
     isFRead = 0; stringFRead = "";
     lastReadFieneryc = millis();
-    
-   }
+  }
 
-   if(isLedOn){
+  if(isLedOn){
       if (millis()- lastReadFieneryc >= ledBlinkTime){
          loff;
          isLedOn = 0;
       }
-   }
+  }
 
-   //range testing...
-  
-   if (millis() - lastReadFieneryc >= 20000){
-    if (millis() - lastWrite >= 1000){
-      Serial.println("OOR OOR");
-      lastWrite = millis();
+  //range testing...
+  if (!MODE) { //If it is operating in normal mode then do this else don't.
+    if (millis() - lastReadFieneryc >= 20000){
+      if (millis() - lastWrite >= 1000){
+        Serial.println("lost contact with LoRa radio sender");
+        lastWrite = millis();
+      }
     }
-   }
+  }
+
+  //prototype signals sending.
+  if (MODE == 9) {
+    if (millis() - prototypeUpdateTime >= prototypeTime){
+      Serial.print(pctr);Serial.println("+RCV=11731,8,u,1,0,1,-99,-40\r\n");
+      pctr = (++pctr)%prototypeTime;
+    }
+  } 
 
 }
